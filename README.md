bd8daceca1331b43b158f897878d75e9c7497e40
firas16 <firasbnsassi@gmail.com>
2022-06-20 10:04:15 +0200

    add readme add getReader unit tests

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# drugExplore
-exploratory analysis of drug mention in publications
+## DrugExplore
+exploratory analysis of drug mention in publications.
+
+## Requirements
+python3, make
+
+## Install
+
+Clone the project into your local machine via:
+
+`git clone https://github.com/firas16/drugExplore.git`
+
+To prepare virtual environment, run inside `drugExplore` directory:
+
+`make venv`
+
+Make sure to activate your virtual environment with:
+
+`. activate`
+
+
+
+    
--- test/readers/test_get_reader.py
+++ test/readers/test_get_reader.py
@@ -1,20 +1,21 @@
 from src.readers.CSVReader import CSVReader
 from src.readers.JSONReader import JSONReader
-
+from src.readers.ReaderFactory import read, get_reader
 from unittest import TestCase
+
 class TestReader(TestCase):
-    def test_get_json_reader(self):
-        # Given
-
-        # When
-
-        # Then
-        ...
-
     def test_read_json(self):
         # Given
-
+        config = {'format': 'json', 'path': '../../test/resources/pubmed.json', 'columns_mapping': {'date': 'date_event'}, 'dates': [{'name': 'date_event', 'format_date': '%Y-%m-%d'}]}
         # When
-
+        reader = get_reader(config)
         # Then
-        ...
+        assert isinstance(reader, JSONReader)
+    def test_read_csv(self):
+        # Given
+        config = {'format': 'csv', 'sep': ',', 'header': 0, 'path': '../../test/resources/drugs.csv', 'columns_mapping': {'date': 'date_event'}, 'dates': [{'name': 'date_event', 'format_date': '%Y-%m-%d'}]}
+        # When
+        reader = get_reader(config)
+        # Then
+        assert isinstance(reader, CSVReader)
+
